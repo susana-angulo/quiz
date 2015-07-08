602c8a66a01a4e679503bb858b5322768d468258
susana-angulo <susana_angulo@yahoo.com>
2015-07-09 00:07:12 +0200

    Express-partial

--- app.js
+++ app.js
@@ -4,18 +4,19 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var partials = require('express-partials');
 
 var routes = require('./routes/index');
 
 var app = express();
-app.listen(3000);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(partials());
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+// app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -24,6 +25,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+
+app.listen(3000)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -58,4 +61,6 @@ app.use(function (err, req, res, next) {
 
 
 module.exports = app;
+
+
 
