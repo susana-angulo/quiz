9d97ec96d58f90f3a7a30f98338ce3ed2e3154a2
susana-angulo <susana_angulo@yahoo.com>
2015-07-09 17:59:25 +0200

    Express - partial version 1.1
    Se modifican ficheros erroneos

--- app.js
+++ app.js
@@ -26,7 +26,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
-app.listen(3000)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
